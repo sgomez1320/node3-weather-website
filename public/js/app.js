43dc41705ad2e7d3257bdd9bd16fd28c8aacc887
Sergio Gomez <sergiogomez@Sergios-MacBook-Pro.local>
2020-01-14 17:22:13 -0500

    Useless space for test

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = ''
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
-            if(data.error){
+            if(data.error) {
                 messageOne.textContent = data.error
             } else {
                 messageOne.textContent = data.location
